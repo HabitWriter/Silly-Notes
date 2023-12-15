6da1486ac918269339aca5e4b1d55b9e49971085
Robert Smith <r.reed.smith@gmail.com>
2023-12-14 20:30:11 -0700

    added models

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,27 @@
 import Subtopic from "./Subtopic.model.js";
+import Topic from "./topic.model.js";
+import Url from "./Urls.model.js";
+import Project from "./Project.model.js";
+import User from "./User.model.js";
+
+// Subtopic and Url relation
+Subtopic.hasMany(Url,  { foreignKey: 'subtopicId' });
+Url.belongsTo(Subtopic, { foreignKey: 'subtopicId' });
+
+// Topic and Subtopic relation
+Topic.hasMany(Subtopic,  { foreignKey: 'topicId' });
+Subtopic.belongsTo(Topic, { foreignKey: 'topicId' });
+
+// Project and Subtopic relation
+Project.belongsToMany(Subtopic, { through: 'ProjectSubtopic' });
+Subtopic.belongsToMany(Project, { through: 'ProjectSubtopic' });
+
+// User to Project relation
+
+
+// User to Topic relation
+
+
 
 // Movie.hasMany(Rating, { foreignKey: 'movieId' });
 // Rating.belongsTo(Movie, { foreignKey: 'movieId' });
@@ -6,4 +29,4 @@ import Subtopic from "./Subtopic.model.js";
 // User.hasMany(Rating, { foreignKey: 'userId' });
 // Rating.belongsTo(User, { foreignKey: 'userId' });
 
-export { Subtopic };
+export { Subtopic, Topic, Url, Project, User };
