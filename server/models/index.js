84b1fd8a11935463c6cdefd1119171eeb84d7feb
Robert Smith <r.reed.smith@gmail.com>
2023-12-14 21:00:48 -0700

    finished relations for models

--- server/models/index.js
+++ server/models/index.js
@@ -17,10 +17,16 @@ Project.belongsToMany(Subtopic, { through: 'ProjectSubtopic' });
 Subtopic.belongsToMany(Project, { through: 'ProjectSubtopic' });
 
 // User to Project relation
-
+User.hasMany(Project,  { foreignKey: 'userId' });
+Project.belongsTo(User, { foreignKey: 'userId' });
 
 // User to Topic relation
+User.hasMany(Topic,  { foreignKey: 'userId' });
+Topic.belongsTo(User, { foreignKey: 'userId' });
 
+// User to Subtopic relation
+User.hasMany(Subtopic,  { foreignKey: 'userId' });
+Subtopic.belongsTo(User, { foreignKey: 'userId' });
 
 
 // Movie.hasMany(Rating, { foreignKey: 'movieId' });
