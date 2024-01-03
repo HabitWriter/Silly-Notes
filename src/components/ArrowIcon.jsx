9e895a497c3e3f89a3e1dcaf54e980985e1ce20f
Robert Smith <r.reed.smith@gmail.com>
2024-01-02 20:25:47 -0700

    fixed errors in the console

--- src/components/ArrowIcon.jsx
+++ src/components/ArrowIcon.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
 import { atom, useAtom } from "jotai";
 
+export const rotationAtom = atom(90)
+
+
 export default function ArrowIcon() {
-    const rotationAtom = atom(0)
+
     const [rotation, setRotation] = useAtom(rotationAtom)
-    
+
 
     function rotateArrow() {
-        rotation === 0 ? setRotation(90): setRotation(0)
+        if (rotation === 0) {
+            setRotation(90)
+        } else {
+            setRotation(0)
+        }
     }
 
 
-
     return (
-        <button title="Open/Close" className={"btn btn-square btn-ghost -rotate-" + rotation} onClick={rotateArrow}>
+        <button title="Open/Close" className={"btn btn-square btn-ghost -rotate-90"}>
             
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
                 width="16"
                 zoomAndPan="magnify"
                 viewBox="0 0 375 374.999991"
@@ -26,26 +31,26 @@ export default function ArrowIcon() {
                 version="1.0"
             >
                 <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     transform="matrix(-0.53033, 0.53033, -0.53033, -0.53033, 384.304402, 110.415682)"
                     fill="none"
-                    stroke-linejoin="miter"
+                    strokeLinejoin="miter"
                     d="M 38.498698 38.501751 L 329.211674 38.498068 "
                     stroke="#737373"
-                    stroke-width="77"
-                    stroke-opacity="1"
-                    stroke-miterlimit="4"
+                    strokeWidth="77"
+                    strokeOpacity="1"
+                    strokeMiterlimit="4"
                 />
                 <path
-                    stroke-linecap="round"
+                    strokeLinecap="round"
                     transform="matrix(0.53033, 0.53033, -0.53033, 0.53033, 31.524271, 69.580272)"
                     fill="none"
-                    stroke-linejoin="miter"
+                    strokeLinejoin="miter"
                     d="M 38.497683 38.499255 L 329.214342 38.499255 "
                     stroke="#737373"
-                    stroke-width="77"
-                    stroke-opacity="1"
-                    stroke-miterlimit="4"
+                    strokeWidth="77"
+                    strokeOpacity="1"
+                    strokeMiterlimit="4"
                 />
             </svg>
         </button>
--- src/components/HeaderDrawer.jsx
+++ src/components/HeaderDrawer.jsx
@@ -14,8 +14,8 @@ export default function HeaderDrawer() {
       {/* <!-- Navbar --> */}
       <div className="w-full navbar bg-base-300">
         <div className="flex-none lg:hidden">
-          <label for="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
           </label>
         </div> 
         <div className="flex-1 px-2 mx-2 text-xl">Silly Notes</div>
@@ -33,7 +33,7 @@ export default function HeaderDrawer() {
 
 
     <div className="drawer-side">
-      <label for="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
+      <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
       <ul className="menu p-4 w-80 min-h-full bg-base-200">
 
         {/* <!-- Sidebar content here --> */}
