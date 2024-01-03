0e8f09db397974ae548ad2ecc6aed1e2d7fea1c0
Robert Smith <r.reed.smith@gmail.com>
2024-01-02 17:04:37 -0700

    Begain work on note card design

--- src/components/SubtopicTable.jsx
+++ src/components/SubtopicTable.jsx
@@ -1,7 +1,33 @@
+import TopicDropdown from "./TopicDropdown";
+
 export default function SubtopicTable() {
-    return (
-        <>
-        <div>Hello</div>
-        </>
-    )
+  return (
+    <div className="w-full p-10">
+      {/* Subtopic Note Card closed */}
+
+      <div className="card w-70% bg-base-300 shadow-xl my-4">
+        
+        <div className="card-body items-center text-center">
+            {/* Flexbox containing title and open button */}
+            <div className="w-full flex justify-between">
+                <div className="w-16"></div>
+                <h2 className="card-title">Div</h2>
+                <button className=" w-16 btn btn-ghost">Open</button>
+            </div>
+
+        </div>
+      </div>
+
+
+      {/* Subtopic Note Card closed */}
+
+      <div className="card w-70% bg-base-300 shadow-xl my-4">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Div</h2>
+          <TopicDropdown />
+          <div className="card-actions"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
